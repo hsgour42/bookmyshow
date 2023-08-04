7bf16b3ad3fc50b26e86dd8fbd0ac41060a12655
hsgour42 <hsgour42@gmail.com>
2023-08-04 08:39:24 +0530

    first version

---
+++ src/main/java/com/scalar/bookmyshow/BookmyshowApplication.java
@@ -0,0 +1,14 @@
+package com.scalar.bookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyshowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmyshowApplication.class, args);
+        System.out.println("");
+    }
+
+}
---
+++ src/test/java/com/scalar/bookmyshow/BookmyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalar.bookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
